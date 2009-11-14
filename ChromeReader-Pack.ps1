baa07d020e35e551c09cc0a67afb819514a3c8b0
jachymko <jachymko@gmail.com>
2009-11-14 17:49:35 +0000

    ChromeReader-Pack.ps1

--- ChromeReader-Pack.ps1
+++ ChromeReader-Pack.ps1
@@ -1,13 +1,47 @@
-if (Test-Path .\tmp)
+function Get-ScriptDirectory
 {
-	Remove-Item .\tmp -Force
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
 }
 
-xcopy .\ChromeReader .\tmp /s /e /i
+Push-Location ( Get-ScriptDirectory )
 
-$ExtDir = (Resolve-Path .\tmp).Path
-$ExtPem = (Resolve-Path .\ChromeReader.pem).Path
+try
+{
+    if (Test-Path ..\tmp)
+    {
+    	Remove-Item ..\tmp -Force -Recurse
+    }
 
-& 'D:\chromium\drops\latest\chrome.exe' "--pack-extension=$ExtDir" "--pack-extension-key=$ExtPem"
+    xcopy . ..\tmp /s /e /i /exclude:FilesToExclude.exclude
 
-#Remove-Item .\tmp -Force -Recurse
+    $ExtDir = (Resolve-Path ..\tmp).Path
+    $ExtPem = (Resolve-Path .\ChromeReader.pem).Path
+
+    $ChromeArgs = 
+    (
+        "--pack-extension=$ExtDir", 
+        "--pack-extension-key=$ExtPem",
+        "--no-message-box"
+    )
+    
+    $chrome = Start-Process 'D:\chromium\drops\latest\chrome.exe' $ChromeArgs -PassThru -Debug -Verbose
+    $chrome.WaitForExit()
+    
+    if (Test-Path ..\tmp.crx)
+    {
+        if (-not (Test-Path ..\ChromeReader_out  -PathType Container))
+        {
+            New-Item ..\ChromeReader_out -ItemType Container
+        }
+        
+        Move-Item ..\tmp.crx ..\ChromeReader_out\ChromeReader.crx
+        Invoke-Item ..\ChromeReader_out
+    }
+    
+    Remove-Item ..\tmp -Force -Recurse
+}
+finally
+{
+    Pop-Location
+}
